cefb75087df3db0cd82aca287151125ad5f1c9df
guiemiliano <guiemiliano.dev@gmail.com>
2024-12-15 20:08:13 -0300

    Alterações para seguir os padrões Spring

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,54 @@
+package dev.java10x.CadastroDeNinjas;
+
+/*
+    Em uma aplicação Spring, usa-se após o nome da classe
+    o padrão que estamos trabalhando, no caso é um modelo
+    de Ninja
+
+    Para criar uma entidade no meu Banco de dados,
+    eu preciso Criar uma ID, pois ela vai ser única
+    e para localizar vai ser muito mais fácil
+ */
+
+
+public class NinjaModel {
+
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
