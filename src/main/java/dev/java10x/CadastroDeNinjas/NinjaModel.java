f9941d456ab033e6bb159e223b26752f47896290
guiemiliano <guiemiliano.dev@gmail.com>
2024-12-15 22:10:49 -0300

    Mensagem de commit

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -15,6 +15,7 @@ import jakarta.persistence.*;
 //Entity transforma uma classe em uma entidade do BD
 //Table transforma uma classe em uma tabela do BD
 //@GeneratedValue(strategy = GenerationType.IDENTITY) gera um id que segue a ordem 1, 2, 3, 4 etc
+//testando gitt
 
 @Entity
 @Table(name = "tb_cadastro")
